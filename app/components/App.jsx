7278c42f3182c61dcbf22b302bdc18f769e83042
Felipe <fchiapini@gmail.com>
2016-04-04 11:38:00 -0300

    Editing tasks

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,66 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
-	render() {
-		return <Note />;
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			notes: [
+				{
+	      	id: uuid.v4(),
+	      	task: 'Learn Webpack'
+	      },
+	      {
+	      	id: uuid.v4(),
+	      	task: 'Learn React'
+	      },
+	      {
+	      	id: uuid.v4(),
+	      	task: 'Do laundry'
+	      }
+			]
+		};
 	}
+
+	render() {
+    
+    const notes = this.state.notes;
+
+    //return <Note />;
+
+    return (
+    	<div>
+    	  <button onClick={this.addNote}>+</button>
+    	  <Notes notes={notes} />
+    	</div>
+    );
+	}
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+      	id: uuid.v4(),
+      	task: 'New task'
+      }])
+    });
+  };
+
+  editNote = (id, task) => {
+  	if(!task.trim()) {
+  		return;
+  	}
+    
+    const notes = this.state.notes.map(note => {
+      if(note.id === id && task) {
+      	note.task = task;
+      }
+
+      return note;
+    });
+
+    this.setState({notes});
+  };
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,74 @@
 import React from 'react';
 
-export default () => <div>Learn Webpack is awesome!</div>;
+export default class Note extends React.Component {
+	constructor(props) {
+		super(props);
+
+		//Track `editing` state.
+		this.state = {
+			editing: false
+		};
+	}
+	render() {
+		//Render the component differently based on state.
+		if(this.state.editing) {
+			return this.renderEdit();
+		}
+
+		return this.renderNote();
+	}
+	renderEdit = () => {
+		// We deal with blur and input handlers here. These map to DOM events.
+    // We also set selection to input end using a callback at a ref.
+    // It gets triggered after the component is mounted.
+    //
+    // We could also use a string reference (i.e., `ref="input") and
+    // then refer to the element in question later in the code. This
+    // would allow us to use the underlying DOM API through
+    // this.refs.input. This can be useful when combined with
+    // React lifecycle hooks.
+    return <input type="text"
+      ref={
+      	(e) => e ? e.selectionStart = this.props.task.length : null
+      }
+      autoFocus={true}
+      defaultValue={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+	};
+	renderNote = () => {
+    // If the user clicks a normal note, trigger editing logic.
+    return <div onClick={this.edit}>{this.props.task}</div>;
+	};
+	edit = () => {
+		// ENter edit mode.
+		this.setState({
+      editing: true
+		});
+	};
+	checkEnter = (e) => {
+		// The user hit *enter*, let's finish up.
+		if(e.key === 'Enter') {
+			this.finishEdit(e);
+		}
+	};
+	finishEdit = (e) => {
+		// `Note` will trigger an optional `onEdit` callback once it
+    // has a new value. We will use this to communicate the change to
+    // `App`.
+    //
+    // A smarter way to deal with the default value would be to set
+    // it through `defaultProps`.
+    //
+    // See the *Typing with React* chapter for more information.
+    const value = e.target.value;
+
+    if(this.props.onEdit) {
+    	this.props.onEdit(value);
+
+    	// Exit edit mode.
+    	this.setState({
+        editing: false
+    	});
+    }
+	};
+}
