6e6b52f7a5cbb5a58e4cd7b502ec7a50c2a865e2
Felipe <fchiapini@gmail.com>
2016-04-18 11:48:36 -0300

    Improvements

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -3,16 +3,16 @@ import alt from '../libs/alt';
 import NoteActions from '../actions/NoteActions';
 
 class NoteStore {
-	constructor() {
-		this.bindActions(NoteActions);
+  constructor() {
+  	this.bindActions(NoteActions);
 
-		this.notes = [];
+  	this.notes = [];
 
-		this.exportPublicMethods({
-      getNotesByIds: this.getNotesByIds.bind(this)
-		});
-	}
-	create(note) {
+  	this.exportPublicMethods({
+    getNotesByIds: this.getNotesByIds.bind(this)
+  	});
+  }
+  create(note) {
     const notes = this.notes;
 
     note.id = uuid.v4();
@@ -22,35 +22,34 @@ class NoteStore {
     });
 
     return note;
-	}
-	update(updateNote) {
+  }
+  update(updateNote) {
 
-		const notes = this.notes.map(note => {
-			if(note.id === updateNote.id) {
-		// Object.assign is used to patch the note data here. It
-        // mutates target (first parameter). In order to avoid that,
-        // I use {} as its target and apply data on it.
-        //
-        // Example: {}, {a: 5, b: 3}, {a: 17} -> {a: 17, b: 3}
-        //
-        // You can pass as many objects to the method as you want.
-        return Object.assign({}, note, updateNote);
-			}
-			return note;
-		});
+  	const notes = this.notes.map(note => {
+  		if(note.id === updateNote.id) {
+  	// Object.assign is used to patch the note data here. It
+      // mutates target (first parameter). In order to avoid that,
+      // I use {} as its target and apply data on it.
+      //
+      // Example: {}, {a: 5, b: 3}, {a: 17} -> {a: 17, b: 3}
+      //
+      // You can pass as many objects to the method as you want.
+      return Object.assign({}, note, updateNote);
+  		}
+  		return note;
+  	});
 
-		// This is same as `this.setState({notes: notes})`
-		this.setState({notes});
+  	// This is same as `this.setState({notes: notes})`
+  	this.setState({notes});
 
-	}
-	delete(id) {
-    
+  }
+  delete(id) {
     this.setState({
     	notes: this.notes.filter(note => note.id !== id)
     });
-	}
-	getNotesByIds(ids) {
-	// 1. Make sure we are operating on an array and
+  }
+  getNotesByIds(ids) {
+    // 1. Make sure we are operating on an array and
     // map over the ids
     // [id, id, id, ...] -> [[Note], [], [Note], ...]
     return (ids || []).map(
@@ -60,7 +59,7 @@ class NoteStore {
     // 3. Filter out possible empty arrays and get notes
     // [[Note], [], [Note]] -> [[Note], [Note]] -> [Note, Note]
     ).filter(a => a.length).map(a => a[0]);
-	}
+  }
 }
 
 export default alt.createStore(NoteStore, 'NoteStore');
